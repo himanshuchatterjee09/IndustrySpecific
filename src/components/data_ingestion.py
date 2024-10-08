34f15a1bcc0f9b807997a88177d744d7eeaf9b8e
Himanshu Chatterjee <himanshu_chatterjee@icloud.com>
2024-10-08 09:28:14 +0100

    Hyper Parameter Training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,10 +6,10 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from src.components.data_transformation import DataTransformation
-from src.components.data_transformation import DataTransformationConfig
 from dataclasses import dataclass
-from src.components.model_trainer import ModelTrainerConfig
+
 from src.components.model_trainer import ModelTrainer
+
 
 @dataclass
 class DataIngestionConfig:
--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -2,7 +2,9 @@ import os
 import sys
 from dataclasses import dataclass
 
+
 from catboost import CatBoostRegressor
+
 from sklearn.ensemble import(
 AdaBoostRegressor,
 GradientBoostingRegressor,
@@ -19,6 +21,7 @@ from xgboost import XGBRegressor
 from src.exception import CustomException
 from src.logger import logging
 from src.utils import save_object,evaluate_models
+import scipy.special
 
 @dataclass
 
@@ -43,11 +46,53 @@ class ModelTrainer:
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
                 "K-Neighbours Classifier": KNeighborsRegressor(),
-                "XGBClassifier": XGBRegressor(),
-                "CatBoosting Classifier": CatBoostRegressor(verbose=False),
-                "AdaBoost Classifier": AdaBoostRegressor()
+                "XGBRegressor": XGBRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor()
             }
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test = X_test,y_test = y_test, models = models)
+            params = {
+                "Decision Tree": {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest": {
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Gradient Boosting": {
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate': [.1, .01, .05, .001],
+                    'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Linear Regression": {},
+                "K-Neighbours Classifier":{
+                    'n_neighbors': [5,7,9,11],
+                    'weights': ['uniform','distance'],
+                    'algorithm':['ball_tree','kd_tree','brute']
+                },
+                "XGBRegressor": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "CatBoosting Regressor": {
+                    'depth': [6, 8, 10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor": {
+                    'learning_rate': [.1, .01, 0.5, .001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                }
+
+            }
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test = X_test,y_test = y_test, models = models,param = params)
             # To get best Model
             best_model_score = max(sorted(model_report.values()))
             # To get the best model from the dict

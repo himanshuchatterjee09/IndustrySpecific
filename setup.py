1f565d120c5f442c69dfb9e6ac53066f53cd4a3e
Himanshu Chatterjee <himanshu_chatterjee@icloud.com>
2024-10-10 09:22:46 +0100

    Bug fix 2

--- setup.py
+++ setup.py
@@ -1,21 +1,25 @@
-from setuptools.command.install import install
+from setuptools import find_packages, setup
 from typing import List
+from setuptools import find_packages,setup
 
-HYPHEN_E_DOT = '-e .'
-def get_requirements(file_path:str)->List[str]:
+HYPEN_E_DOT = '-e .'
+
+
+def get_requirements(file_path: str) -> List[str]:
     '''
-
     this function will return the list of requirements
     '''
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
-        [req.replace("/n"," ") for req in requirements]
-        if HYPHEN_E_DOT in requirements:
-            requirements.remove(HYPHEN_E_DOT)
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
     return requirements
 
-from setuptools import find_packages,setup
+
 setup(
     name = 'IndustrySpecific',
     version = '0.0.1',
